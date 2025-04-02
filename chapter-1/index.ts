4128ec5e40141737665f629461a4efbe68b821cd
Raymond <akdfhr2@gmail.com>
2025-04-02 23:59:33 +0900

    Chapter1: playFor 질의함수로 바꾸기

--- chapter-1/index.ts
+++ chapter-1/index.ts
@@ -15,31 +15,34 @@ interface Invoice {
 
 type Plays = Record<string, PlayInfo>;
 
-const amountFor = (perf: Performance, play: PlayInfo) => {
-  let thisAmount = 0;
-
-  switch (play.type) {
-    case "tragedy":
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
-      }
-      break;
-    case "comedy":
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
-      }
-      thisAmount += 300 * perf.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${play.type}`);
-  }
-
-  return thisAmount;
-};
-
 function statement(invoice: Invoice, plays: Plays) {
+  const playFor = (aPerformance: Performance) => {
+    return plays[aPerformance.playID];
+  };
+
+  const amountFor = (aPerformance: Performance, play: PlayInfo) => {
+    let result = 0;
+
+    switch (play.type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${play.type}`);
+    }
+
+    return result;
+  };
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
@@ -50,8 +53,7 @@ function statement(invoice: Invoice, plays: Plays) {
   }).format;
 
   for (const perf of invoice.performances) {
-    const play = plays[perf.playID as keyof typeof plays];
-    const thisAmount = amountFor(perf, play);
+    const thisAmount = amountFor(perf, playFor(perf));
 
     totalAmount += thisAmount;
 
@@ -59,10 +61,11 @@ function statement(invoice: Invoice, plays: Plays) {
     volumeCredits += Math.max(perf.audience - 30, 0);
 
     // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
 
     // 이 줄은 청구 내역을 출력한다.
-    result += `${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
+    result += `${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
   }
 
   result += `총액 ${format(totalAmount / 100)}\n`;
