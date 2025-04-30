66f23f30906d5a897e445c0eb557f93f3ee65f6a
raymondanythings <raymondanything@gmail.com>
2025-04-30 23:54:43 +0900

    Chapter7: 예제 코드 추가

---
+++ chapter-7/exmaple.ts
@@ -0,0 +1,6 @@
+const organization = { name: "애크미 구스베리", country: "GB" };
+
+let result = "";
+
+result += `<h1>${organization.name}</h1>`;
+organization.name = "애크미 구스베리";
---
+++ chapter-7/index.ts
@@ -0,0 +1,28 @@
+class Orgnization {
+  private _name: string;
+  private _country: string;
+
+  constructor(data: {
+    name: string;
+    country: string;
+  }) {
+    this._name = data.name;
+    this._country = data.country;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value: string) {
+    this._name = value;
+  }
+
+  get country() {
+    return this._country;
+  }
+
+  set country(value: string) {
+    this._country = value;
+  }
+}
